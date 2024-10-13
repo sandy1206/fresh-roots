8c522d3aa865b22d23513b57fbe2bb52bf465c26
Sandip Arsude <arsudesandip4@gmail.com>
2024-10-13 22:57:34 +0530

    added about

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { AboutUs } from "./components/AboutUs";
 import { Hero } from "./components/Hero";
-import { WhyChooseFreshRoots } from "./components/WhyUS";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
-			<AboutUs/>
-			<WhyChooseFreshRoots/>
 		</>
 	);
 }
